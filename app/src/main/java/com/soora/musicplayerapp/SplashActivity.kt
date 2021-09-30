7198899e740c5ca4523ce71b49586f079be7ade0
soora <sr9305@gmail.com>
2021-09-30 22:53:33 +0900

    Make splash activity

---
+++ app/src/main/java/com/soora/musicplayerapp/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.soora.musicplayerapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
